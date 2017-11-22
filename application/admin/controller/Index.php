dafc08c9657588e992e0f33be72634dc6b379435
webjust <wbo86@126.com>
2017-11-23 03:01:47 +0800

    新增访问页面数量统计

--- application/admin/controller/Index.php
+++ application/admin/controller/Index.php
@@ -2,13 +2,26 @@
 
 namespace app\admin\controller;
 
-use app\common\model\Admin;
+use app\common\model\Log as LogModel;
 
 class Index extends Base
 {
     public function index()
     {
-        return $this->fetch();
+        // 浏览页面数量
+        $yesterday = LogModel::viewPageCount(1);  // 昨天
+        $week = LogModel::viewPageCount('week');            // 上一周
+        $month = LogModel::viewPageCount(30);          // 过去30天
+        $days = [
+            LogModel::viewPageCount(1),
+            LogModel::viewPageCount(2),
+            LogModel::viewPageCount(3),
+            LogModel::viewPageCount(4),
+            LogModel::viewPageCount(5),
+            LogModel::viewPageCount(6),
+            LogModel::viewPageCount(7),
+        ];
+        return $this->fetch('', compact('yesterday', 'week', 'month', 'days'));
     }
 
     public function welcome()
--- application/common/model/Log.php
+++ application/common/model/Log.php
@@ -17,10 +17,39 @@ class Log extends Model
         }
     }
 
+    /*日志列表*/
     public static function getList($condition = array(), $size = 10, $order = "log_id desc")
     {
         $list = self::where($condition)->limit($size)->order($order)->paginate($size);
         $page = $list->render();
         return [$list, $page];
+    }
+
+    public static function viewPageCount($day = 1)
+    {
+        $condition = [];
+        if (is_int($day)) {
+            $end = strtotime('yesterday') + 60 * 60 * 24;
+            $begin = $end - ($day) * 60 * 60 * 24;
+            $condition['create_time'] = [['>', $begin], ['<', $end], 'and'];
+        }
+        if ($day == 'week') {
+            // 上周周一
+            $last_week_begin = strtotime(date('Y-m-d', strtotime('last week')));
+            // 上周周日
+            $last_week_end = strtotime(date('Y-m-d', strtotime('last week'))) + 3600 * 24 * 7;
+            $condition['create_time'] = [['>', $last_week_begin], ['<', $last_week_end], 'and'];
+        }
+        if ($day == 'month') {
+            // 过去一个月
+            $last_month_begin = strtotime('last month');
+            $last_month_end = $last_month_begin + 3600 * 24 * 30;
+            $condition['create_time'] = [['>', $last_month_begin], ['<', $last_month_end], 'and'];
+        }
+/*        self::where($condition)->count();
+        echo self::getLastSql();
+        echo '<br>';
+        echo date('Y-m-d H:i:s', strtotime('last month'));*/
+        return self::where($condition)->count();
     }
 }
